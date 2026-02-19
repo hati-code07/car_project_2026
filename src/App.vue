1890e9b7e0516a1f369324a92a8c7c767673777b
maria123 <khatidzea@gmail.com>
2026-02-19 10:49:04 +0300

    Верстка в drawer

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,73 @@
 <template>
-<v-app>
-<v-navigation-drawer app v-model="drawer">
-
-    <!-- Первый блок - Заголовок -->
-    <v-list-item>
-      <v-list-item-content>
-        <v-list-item-title class="title">
-          КИПУ
-        </v-list-item-title>
-        <v-list-item-subtitle>
-          Учебный проект
-        </v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-
-    <!-- Разделитель -->
-    <v-divider></v-divider>
-
-    <!-- Навигационный список -->
-    <v-list dense>
-      <!-- Первый пункт меню -->
+  <v-app>
+    <!-- Навигационный ящик (drawer) -->
+    <v-navigation-drawer app v-model="drawer">
+      <!-- Первый блок - Заголовок -->
       <v-list-item>
-        <template v-slot:prepend>
-          <v-icon icon="mdi-cake-variant"></v-icon>
-        </template>
-        <v-list-item-title>Link One</v-list-item-title>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            КИПУ
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Учебный проект
+          </v-list-item-subtitle>
+        </v-list-item-content>
       </v-list-item>
 
-      <!-- Второй пункт меню -->
-      <v-list-item>
-        <template v-slot:prepend>
-          <v-icon icon="mdi-home"></v-icon>
-        </template>
-        <v-list-item-title>Главная</v-list-item-title>
-      </v-list-item>
+      <!-- Разделитель -->
+      <v-divider></v-divider>
 
-      <!-- Третий пункт меню -->
-      <v-list-item>
-        <template v-slot:prepend>
-          <v-icon icon="mdi-account"></v-icon>
-        </template>
-        <v-list-item-title>Профиль</v-list-item-title>
-      </v-list-item>
+      <!-- Навигационный список (динамический из массива links) -->
+      <v-list dense>
+        <v-list-item
+          v-for="link in links"
+          :key="link.title"
+        >
+          <template v-slot:prepend>
+            <v-icon :icon="link.icon"></v-icon>
+          </template>
+          <v-list-item-title>{{ link.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
-      <!-- Добавьте свои пункты меню -->
-    </v-list>
-</v-navigation-drawer>
-<v-app-bar app dark color="primary">
-<v-app-bar-nav-icon  @click="drawer = !drawer"></v-app-bar-nav-icon>
-<v-spacer></v-spacer>
-<v-toolbar-items class="hidden-sm-and-down">
-<v-btn>
-    <v-icon
-    start
-    icon="mdi-cake-variant"
-    ></v-icon>
-    Link One
-</v-btn>
-<v-btn>Link Two</v-btn>
-<v-btn>Link Three</v-btn>
-</v-toolbar-items>
-</v-app-bar>
-<v-main>
-</v-main>
-</v-app>
+    <!-- Верхняя панель -->
+    <v-app-bar app dark color="primary">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      
+      <v-spacer></v-spacer>
+      
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn
+          v-for="link in links"
+          :key="link.title"
+          text
+        >
+          <v-icon start :icon="link.icon"></v-icon>
+          {{ link.title }}
+        </v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+
+    <!-- Основной контент -->
+    <v-main>
+    </v-main>
+  </v-app>
 </template>
-
 
 <script>
 export default {
- data() {
+  data() {
     return {
       drawer: false,
+      links: [
+        {title: "Login", icon: "mdi-lock", url: "/login"},
+        {title: "Registration", icon: "mdi-face", url: "/registration"},
+        {title: "Orders", icon: "mdi-bookmark-multiple-outline", url: "/orders"},
+        {title: "New ad", icon: "mdi-note-plus-outline", url: "/new"},
+        {title: "My ads", icon: "mdi-view-list-outline", url: "/list"}
+      ]
     }
   }
-
 }
 </script>
