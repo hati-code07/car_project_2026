b26290f51dc28f31909db8c926cfe71aecec6261
maria123 <khatidzea@gmail.com>
2026-02-19 10:58:59 +0300

    Создание всех страниц

---
+++ src/views/Ads/NewAdView.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <h1>Новое объявление</h1>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  }
+}
+</script>
