272c08600e81a5ca772ae7de327572b0eb63379b
maria123 <khatidzea@gmail.com>
2026-02-19 10:28:38 +0300

    Добавление drawer

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
 <v-app>
-<v-navigation-drawer app></v-navigation-drawer>
+<v-navigation-drawer app v-model="drawer"></v-navigation-drawer>
 <v-app-bar app dark color="primary">
-<v-app-bar-nav-icon></v-app-bar-nav-icon>
+<v-app-bar-nav-icon  @click="drawer = !drawer"></v-app-bar-nav-icon>
 <v-spacer></v-spacer>
 <v-toolbar-items class="hidden-sm-and-down">
 <v-btn>
-<v-icon
-start
-icon="mdi-cake-variant"
-></v-icon>
-Link One
+    <v-icon
+    start
+    icon="mdi-cake-variant"
+    ></v-icon>
+    Link One
 </v-btn>
 <v-btn>Link Two</v-btn>
 <v-btn>Link Three</v-btn>
