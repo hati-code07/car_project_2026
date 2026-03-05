28cca8abe8ba5d2a3190103799a4b8b9e29c4705
maria123 <khatidzea@gmail.com>
2026-03-05 10:06:06 +0300

    Верстка форма

--- src/views/Auth/LoginView.vue
+++ src/views/Auth/LoginView.vue
@@ -1,16 +1,40 @@
 <template>
   <v-container>
-    <v-row justify="center">
-      <h1>Вход в систему</h1>
-    </v-row>
-  </v-container>
+  <v-row justify="center">
+  <v-col cols="12" sm="8" lg="6">
+      <v-card class="elevetion-12" >
+      <v-toolbar dark color="primary" class="pl-4" > Login
+  </v-toolbar>
+  <v-card-text>
+  <v-form>
+  <v-text-field
+      prepend-icon="mdi-account"
+      name="email"
+      label="Email"
+      type="email">
+  </v-text-field>
+  <v-text-field
+      prepend-icon="mdi-lock"
+      name="password"
+      label="Password"
+      type="password">
+  </v-text-field>
+  </v-form>
+  </v-card-text>
+  <v-card-actions>
+    <v-spacer></v-spacer>
+    <v-btn color="primary">Login</v-btn>
+    </v-card-actions>
+  </v-card>
+  </v-col>
+  </v-row>
+</v-container>
 </template>
-
 <script>
 export default {
-  data() {
+  data () {
     return {
-    }
+            }
   }
 }
 </script>
