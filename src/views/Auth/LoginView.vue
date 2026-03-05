5e03eee760fdfb404a60621642d40868e7e7353f
maria123 <khatidzea@gmail.com>
2026-03-05 10:38:24 +0300

    Submit  формы

--- src/views/Auth/LoginView.vue
+++ src/views/Auth/LoginView.vue
@@ -32,8 +32,8 @@
     <v-spacer></v-spacer>
     <v-btn 
      color="primary"
-    :disabled="!valid"
-    @click="login">Login</v-btn>
+    @click="onSubmit"
+    :disabled="!valid">Login</v-btn>
     </v-card-actions>
   </v-card>
   </v-col>
@@ -58,7 +58,20 @@ export default {
 	        v => (v && v.length >= 6) || 'Password must 			be more or equel than 6 characters'
 	        ]
 		} 	
-	}
+	},
+  methods: {
+	  onSubmit(){
+      if (this.$refs.form.validate()){
+        const user = {
+				email: this.email,
+				password: this.password
+			}
+			console.log(user)
+		}
+
+    }
+}
+
   
 
 }
