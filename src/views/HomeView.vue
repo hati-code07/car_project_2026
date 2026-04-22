077edfd54182170d1c615e1a2ad7f5e7d223efe1
maria123 <khatidzea@gmail.com>
2026-04-22 20:06:34 +0300

    ready home pge

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,6 +6,9 @@
     <v-col cols="12" xs="12">
         <v-carousel>
     <v-carousel-item 
+    v-for="ad in ads.filter(ad => ad.promo === true)"
+                        :key="ad.id"
+                        :src="ad.src"
     cover
     ></v-carousel-item>
     </v-carousel>
@@ -15,18 +18,23 @@
 <v-container>
   <v-row justify="center">
   <v-col cols="12" xs="12">
+    v-for="ad in ads"
+                :key="ad.id"
 
   <v-card>
         <v-img
     height="200px"
+    :src="ad.src"
       ></v-img>
   <v-card-title primary-title>
+
 <div>
-<h3
-class="headline mb0"
->Здесь будет заголовок</h3>
-<div>Здесь будет описание</div>
-</div>
+
+                            <!-- ЗДЕСЬ меняем: статические тексты на динамические -->
+                            <h3 class="headline mb0">{{ ad.title }}</h3>
+                            <div>{{ ad.desc }}</div>
+                        </div>
+
   </v-card-title>
   <v-card-actions>
     <v-spacer></v-spacer>
@@ -40,3 +48,42 @@ class="headline mb0"
 </v-row>
 </v-container>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            ads: [
+                {
+                    title: "First",
+                    desc: "First Desc",
+                    promo: true,
+                    src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+                    id: "1"
+                },
+                {
+                    title: "Second",
+                    desc: "Second Desc",
+                    promo: true,
+                    src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+                    id: "2"
+                },
+                {
+                    title: "Third",
+                    desc: "Third Desc",
+                    promo: true,
+                    src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+                    id: "3"
+                },
+                {
+                    title: "Fourth",
+                    desc: "Fourth Desc",
+                    promo: true,
+                    src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+                    id: "4"
+                }
+            ]
+        }
+    }
+}
+</script>
