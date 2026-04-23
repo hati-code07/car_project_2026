b0891999d96ac0743623a3a0af13ffbc50fb57e5
maria123 <khatidzea@gmail.com>
2026-04-23 08:15:15 +0300

    New HomeVue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,52 +1,63 @@
-
-
 <template>
     <v-container>
-    <v-row justify="center">
-    <v-col cols="12" xs="12">
-        <v-carousel>
-    <v-carousel-item 
-    v-for="ad in ads.filter(ad => ad.promo === true)"
+        <!-- Карусель с промо-объявлениями -->
+        <v-row justify="center">
+            <v-col cols="12" xs="12">
+                <v-carousel>
+                    <v-carousel-item 
+                        v-for="ad in ads.filter(ad => ad.promo === true)"
                         :key="ad.id"
                         :src="ad.src"
-    cover
-    ></v-carousel-item>
-    </v-carousel>
-</v-col>
-</v-row>
-</v-container>
-<v-container>
-  <v-row justify="center">
-  <v-col cols="12" xs="12">
-    v-for="ad in ads"
+                        cover
+                    >
+                        <!-- КНОПКА ВНУТРИ КАРУСЕЛИ -->
+                        <div class="ad-link">
+                            <v-btn class="error" :to="'/ad/' + ad.id">
+                                {{ ad.title }}
+                            </v-btn>
+                        </div>
+                    </v-carousel-item>
+                </v-carousel>
+            </v-col>
+        </v-row>
+    </v-container>
+
+    <!-- Сетка с объявлениями -->
+    <v-container grid-list-lg>
+        <v-layout row wrap>
+            <v-flex 
+                xs12 
+                sm6 
+                md4
+                v-for="ad in ads"
                 :key="ad.id"
-
-  <v-card>
-        <v-img
-    height="200px"
-    :src="ad.src"
-      ></v-img>
-  <v-card-title primary-title>
-
-<div>
-
-                            <!-- ЗДЕСЬ меняем: статические тексты на динамические -->
-                            <h3 class="headline mb0">{{ ad.title }}</h3>
+            >
+                <v-card>
+                    <v-img
+                        :src="ad.src"
+                        height="200px"
+                    ></v-img>
+                    
+                    <v-card-title primary-title>
+                        <div>
+                            <h3 class="headline mb-0">{{ ad.title }}</h3>
                             <div>{{ ad.desc }}</div>
                         </div>
-
-  </v-card-title>
-  <v-card-actions>
-    <v-spacer></v-spacer>
-    <v-btn text>Open</v-btn>
-    <v-btn raised color="primary">
-              Buy
-</v-btn>
-</v-card-actions>
-</v-card>
-</v-col>
-</v-row>
-</v-container>
+                    </v-card-title>
+                    
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn text :to="'/ad/' + ad.id">
+                            Open
+                        </v-btn>
+                        <v-btn raised color="primary">
+                            Buy
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
@@ -87,3 +98,16 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.ad-link {
+    position: absolute;
+    bottom: 50px;
+    left: 50%;
+    background: rgba(0, 0, 0, 0.5);
+    transform: translate(-50%, 0);
+    padding: 5px 15px;
+    border-top-right-radius: 5px;
+    border-top-left-radius: 5px;
+}
+</style>
