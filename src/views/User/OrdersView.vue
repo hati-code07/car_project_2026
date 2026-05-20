45ba3793a7de955fe8a36fe3654b291e3e8da2b3
maria123 <khatidzea@gmail.com>
2026-05-20 12:34:12 +0300

    orders

--- src/views/User/OrdersView.vue
+++ src/views/User/OrdersView.vue
@@ -4,8 +4,29 @@
   <v-col cols="12" sm="8" lg="6">
       <h1 class="text--secondary mb-3 mt-3">Orders</h1>
       <v-list subheader two-line flat>
+       <v-list-item v-for="order in orders" 
+	:key="order.id">
+<template v-slot:prepend>
+<v-list-item-action>
+<v-checkbox
+:input-value="order.done"
+color="primary"
+></v-checkbox>
+</v-list-item-action>
+</template>
+<v-list-item-title>{{ order.name }}</v-list-item-title>
+<v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
+
+<template v-slot:append>
+<v-list-item-action>
+<v-btn class="primary" :to="'/ad/' + order.adId">Open</v-btn>
+</v-list-item-action>
+</template>
+</v-list-item>
+ 
     <v-list-item>
       <template v-slot:prepend>
+
       <v-list-item-action>
       <v-checkbox
       input-value="false"
